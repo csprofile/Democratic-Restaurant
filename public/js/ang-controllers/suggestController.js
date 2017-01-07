d90221e2f98f8ad409e956d4ac45f3e8f2013740
Cristian Silva <wd.cristian@gmail.com>
2017-01-06 22:27:43 -0200

    Fixing user feedback in suggestions

--- public/js/ang-controllers/suggestController.js
+++ public/js/ang-controllers/suggestController.js
@@ -1,16 +1,20 @@
 'use strict'
 
 app.controller('suggestController',['$scope', '$http',function($scope,$http){
+    $scope.showMessage = false;
 
     $scope.suggest = function(){
         $http.post('/api/restaurant',{name:$scope.restaurantName, address:$scope.restaurantAddress}).
         success(function(data) {
-            console.log("posted successfully");
-            console.log($scope);
+            $scope.message = "Your suggestion was included";
+            $scope.messageClass = "success";
+            $scope.showMessage = true;
         }).error(function(data) {
-            console.error(data);
+            $scope.message = "An error has occurred while creating your suggestion, try again latter.";
+            $scope.messageClass = "danger";
+            $scope.showMessage = false;
         })
-        
+
         $scope.restaurantName = '';
         $scope.restaurantAddress = '';
     };
